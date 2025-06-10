ffe7de938b230c5a3c147de231d4523c2302bb15
Luc Grosheintz <luc.grosheintz@gmail.com>
2025-06-10 21:58:03 +0200

    fix: error condition h5p_get_file_space_strategy. (#45)
    The library seems to only return 0 on success. However, the
    documentation states only negative values imply failure.
    
    Also fixes the error message `s/setting/getting`.

--- include/highfive/bits/h5p_wrapper.hpp
+++ include/highfive/bits/h5p_wrapper.hpp
@@ -32,8 +32,8 @@ inline herr_t h5p_get_file_space_strategy(hid_t plist_id,
                                           hbool_t* persist,
                                           hsize_t* threshold) {
     herr_t err = H5Pget_file_space_strategy(plist_id, strategy, persist, threshold);
-    if (err) {
-        HDF5ErrMapper::ToException<PropertyException>("Error setting file space strategy.");
+    if (err < 0) {
+        HDF5ErrMapper::ToException<PropertyException>("Error getting file space strategy.");
     }
 
     return err;
